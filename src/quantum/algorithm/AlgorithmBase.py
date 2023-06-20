bd592d7201308f034b4d1afa7f65464a76036b4a
Tasdiqul <tasdiq088@gmail.com>
2023-06-20 14:29:50 -0700

    swap twice

--- src/quantum/algorithm/AlgorithmBase.py
+++ src/quantum/algorithm/AlgorithmBase.py
@@ -30,7 +30,7 @@ class AlgorithmResult:
     def Avg(results: list):
         AvgResult = AlgorithmResult()
 
-        ttime = 11
+        ttime = 101
         AvgResult.remainRequestPerRound = [0 for _ in range(ttime)]
         for result in results:
             AvgResult.algorithmRuntime += result.algorithmRuntime
