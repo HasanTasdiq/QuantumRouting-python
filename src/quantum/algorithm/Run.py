144da3910b95d097715eaba4a2e56ad13a9e5e4d
Tasdiqul <tasdiq088@gmail.com>
2023-07-20 15:00:46 -0700

    SEE

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -45,7 +45,7 @@ def Run(numOfRequestPerRound = 5, numOfNode = 100, r = 7, q = 0.9, alpha = 0.000
 
     # make copy
     algorithms = []
-    algorithms.append(MyAlgorithm(copy.deepcopy(topo)))
+    # algorithms.append(MyAlgorithm(copy.deepcopy(topo)))
     # algorithms.append(SEERCACHE(copy.deepcopy(topo), param = 'ten'))
     # algorithms.append(SEERCACHE2(copy.deepcopy(topo), param = 'ten'))
 
@@ -63,13 +63,13 @@ def Run(numOfRequestPerRound = 5, numOfNode = 100, r = 7, q = 0.9, alpha = 0.000
     
     algorithms.append(REPS(copy.deepcopy(topo)))
     # algorithms.append(REPSCACHE(copy.deepcopy(topo),param='ten',name='REPSCACHE'))
-    algorithms.append(REPSCACHE2(copy.deepcopy(topo),param='ten',name='REPSCACHE2'))
-    algorithms.append(SEE2(copy.deepcopy(topo)))
+    # algorithms.append(REPSCACHE2(copy.deepcopy(topo),param='ten',name='REPSCACHE2'))
+    algorithms.append(SEE(copy.deepcopy(topo)))
 
     algorithms[0].r = r
     algorithms[0].density = SocialNetworkDensity
 
-    times = 3
+    times = 2
     # times = 10
     results = [[] for _ in range(len(algorithms))]
     ttime = rtime
@@ -255,7 +255,7 @@ if __name__ == '__main__':
     # print('--DONE--')
     # exit(0)
     # write remainRequestPerRound
-    rtime = 201
+    rtime = 51
     print('starting.. ')
     # sampleRounds = [0, 2, 4, 6, 8, 10]
     sampleRounds = [i for i in range(0 , rtime , int(rtime/5))]
