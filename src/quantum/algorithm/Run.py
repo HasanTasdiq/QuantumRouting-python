7bf7c18c4558443ae7c1c7a2e11827fe41124bd5
Tasdiqul Islam <tasdiqulislam@Tasdiquls-MBP.dhost.uta.edu>
2023-09-05 14:52:04 -0500

    run

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -37,7 +37,7 @@ def runThread(algo, requests, algoIndex, ttime, pid, resultDict):
 
 
 
-def Run(numOfRequestPerRound = 5, numOfNode = 100, r = 7, q = 0.9, alpha = 0.0002, SocialNetworkDensity = 0.5, rtime = 101, topo = None, FixedRequests = None , results=[]):
+def Run(numOfRequestPerRound = 15, numOfNode = 100, r = 7, q = 0.9, alpha = 0.0002, SocialNetworkDensity = 0.5, rtime = 101, topo = None, FixedRequests = None , results=[]):
 
     if topo == None:
         topo = Topo.generate(numOfNode, q, 5, alpha, 6)
