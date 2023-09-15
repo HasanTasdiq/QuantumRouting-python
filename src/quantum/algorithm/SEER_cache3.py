96f05c1c54078f90dcd1625d9bfddf991e08f919
Tasdiqul Islam <tasdiqulislam@cse-137560.lan>
2023-09-15 16:26:36 -0500

    seer cache 2 3

--- src/quantum/algorithm/SEER_cache3.py
+++ src/quantum/algorithm/SEER_cache3.py
@@ -598,8 +598,8 @@ class SEERCACHE3(AlgorithmBase):
                 for (l1, l2) in zip(prevLinks, nextLinks):
                     # usedLinks.add(l1)
                     # usedLinks.add(l2)
-                    # swapped = curr.attemptSwapping2(l1, l2 , timeSlot = self.timeSlot)
-                    swapped = curr.attemptSwapping(l1, l2 )
+                    swapped = curr.attemptSwapping2(l1, l2 , timeSlot = self.timeSlot)
+                    # swapped = curr.attemptSwapping(l1, l2 )
                     if swapped:
                         attemptedLinks.add(l1)
                         attemptedLinks.add(l2)
