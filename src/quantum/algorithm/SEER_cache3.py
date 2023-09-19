9135d08eff580627d2c09ad81b2bd39c55c79f2f
Tasdiqul Islam <tasdiqulislam@cse-137560.lan>
2023-09-19 16:19:09 -0500

    reps run q and alpha

--- src/quantum/algorithm/SEER_cache3.py
+++ src/quantum/algorithm/SEER_cache3.py
@@ -718,7 +718,7 @@ class SEERCACHE3(AlgorithmBase):
     
 if __name__ == '__main__':
 
-    topo = Topo.generate(100, 0.9, 5, 0.0002, 6)
+    topo = Topo.generate(100, 0.8, 5, 0.002, 6)
     s = SEERCACHE3(topo , preEnt=False, param='ten')
     
     # for i in range(0, 200):
