13c1e6d251ed2a4b984c681804943a8c0345b7d7
Tasdiqul Islam <tasdiqulislam@cse-137560.lan>
2023-09-23 20:06:20 -0500

    seer

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
                 tmp_ids[i].append((a[0], a[1]))
                
 
-    skipXlabel = [  0,1, 2, 3, 4,5, 6]
+    skipXlabel = [  0,1, 2, 3,5, 6, 7]
     for XlabelIndex in range(len(Xlabels)):
         # continue
         Xlabel = Xlabels[XlabelIndex]
