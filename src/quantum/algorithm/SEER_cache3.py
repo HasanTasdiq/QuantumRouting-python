fee8f0de738dd930b7bc261d42e7b6159365fac2
Tasdiqul <tasdiq088@gmail.com>
2023-10-07 14:45:11 -0500

    seer without re use

--- src/quantum/algorithm/SEER_cache3.py
+++ src/quantum/algorithm/SEER_cache3.py
@@ -607,8 +607,8 @@ class SEERCACHE3(AlgorithmBase):
                 for (l1, l2) in zip(prevLinks, nextLinks):
                     # usedLinks.add(l1)
                     # usedLinks.add(l2)
-                    swapped = curr.attemptSwapping2(l1, l2 , timeSlot = self.timeSlot)
-                    # swapped = curr.attemptSwapping(l1, l2)
+                    # swapped = curr.attemptSwapping2(l1, l2 , timeSlot = self.timeSlot)
+                    swapped = curr.attemptSwapping(l1, l2)
                     key = (curr , l1.theOtherEndOf(curr) , l2.theOtherEndOf(curr))
                     if not key in self.topo.needLinksDict:
                         self.topo.needLinksDict[key] = ([self.timeSlot])
