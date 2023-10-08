7912c13cd7b9643d2733399665fa3eed36142be4
Tasdiqul <tasdiq088@gmail.com>
2023-10-08 11:39:45 -0500

    biased

--- src/quantum/algorithm/test.py
+++ src/quantum/algorithm/test.py
@@ -2,9 +2,20 @@ import os
 import gurobipy as gp
 from gurobipy import quicksum
 import networkx as nx
+import numpy as np
 
-algoNames = ['SEER1' , 'SEER2']
+# algoNames = ['SEER1' , 'SEER2']
 
-print('SEER' in algoNames[0])
+# print('SEER' in algoNames[0])
 
 
+bias_weights = [x%10==0 for x in range(100)]
+prob = np.array(bias_weights) / np.sum(bias_weights)
+print(prob)
+
+for _ in range(20):
+    sample_size = 2
+    choice_indices = np.random.choice(len(prob), size=sample_size, replace=False, p=prob)
+
+# Get corresponding paths
+    print(choice_indices)
