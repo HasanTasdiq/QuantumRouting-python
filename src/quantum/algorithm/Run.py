25ecd3e3b300b6709c17f884f2c301d4c4b41367
Tasdiqul <tasdiq088@gmail.com>
2023-10-11 13:20:01 -0500

    fix bug for seer3

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -41,7 +41,7 @@ def runThread(algo, requests, algoIndex, ttime, pid, resultDict):
 
 
 
-def Run(numOfRequestPerRound = 3, numOfNode = 18, r = 7, q = 0.8, alpha = 0.002, SocialNetworkDensity = 0.5, rtime = 101, topo = None, FixedRequests = None , results=[]):
+def Run(numOfRequestPerRound = 3, numOfNode = 100, r = 7, q = 0.8, alpha = 0.0002, SocialNetworkDensity = 0.5, rtime = 101, topo = None, FixedRequests = None , results=[]):
 
     if topo == None:
         topo = Topo.generate(numOfNode, q, 5, alpha, 6)
