d42c838f0e4a63534739d63b9396c6265cf7d3aa
Tasdiqul <tasdiq088@gmail.com>
2023-11-06 13:05:55 -0600

    seer

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -42,7 +42,7 @@ def runThread(algo, requests, algoIndex, ttime, pid, resultDict):
 
 
 
-def Run(numOfRequestPerRound = 20, numOfNode = 100, r = 7, q = 0.8, alpha = 0.0002, SocialNetworkDensity = 0.5, rtime = 100, topo = None, FixedRequests = None , results=[]):
+def Run(numOfRequestPerRound = 20, numOfNode = 50, r = 7, q = 0.8, alpha = 0.0002, SocialNetworkDensity = 0.5, rtime = 100, topo = None, FixedRequests = None , results=[]):
 
     if topo == None:
         topo = Topo.generate(numOfNode, q, 5, alpha, 6)
