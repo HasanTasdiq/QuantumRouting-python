3e34d974f3caebd724635fb45ff8f80c0f662c72
Tasdiqul <tasdiq088@gmail.com>
2023-12-14 21:50:50 -0600

    seer ent lifetime

--- src/quantum/algorithm/AlgorithmBase.py
+++ src/quantum/algorithm/AlgorithmBase.py
@@ -39,7 +39,7 @@ class AlgorithmResult:
     def Avg(results: list , requestPerRound = 0):
         AvgResult = AlgorithmResult()
 
-        ttime = len(result.remainRequestPerRound)
+        ttime = len(results[0].remainRequestPerRound)
         AvgResult.remainRequestPerRound = [0 for _ in range(ttime)]
         AvgResult.entanglementPerRound = [0 for _ in range(ttime)]
         for result in results:
