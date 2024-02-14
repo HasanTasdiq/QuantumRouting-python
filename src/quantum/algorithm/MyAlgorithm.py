2aa881e74dd9c396c4bb7bfad8559ac88d798e79
Hasan Tasdiq <tasdiq088@Gmail.com>
2024-02-13 19:06:50 -0600

    seer

--- src/quantum/algorithm/MyAlgorithm.py
+++ src/quantum/algorithm/MyAlgorithm.py
@@ -382,109 +382,7 @@ class MyAlgorithm(AlgorithmBase):
             if not found:
                 break
         # while end
-    # def p2Extra2(self):
-
-    #     while True:
-    #         found = False
-
-    #         for req in self.requestState:
-    #             requestInfo = self.requestState[req]
-    #             pathseg = []
-    #             if requestInfo.state == 0: 
-    #                 src, dst = req[0], req[1]
-    #             elif requestInfo.state == 1:
-    #                 src, dst = req[0], requestInfo.intermediate
-    #                 pathseg = requestInfo.pathseg1
-
-    #             elif requestInfo.state == 2:
-    #                 src, dst = requestInfo.intermediate, req[1]
-    #                 pathseg = requestInfo.pathseg2
-                
-    #             if self.getTotalPathSuccessProb(pathseg) >= 1:
-    #                 continue
-
-    #             if True:
-    #             # if not requestInfo.taken:
-
-    #                 if src.remainingQubits < 1:
-    #                     continue
-    #                 p = []
-    #                 p.append(src)
-                    
-    #                 # Find a shortest path by greedy min hop  
-    #                 while True:
-    #                     last = p[-1]
-    #                     if last == dst:
-    #                         break
-
-    #                     # Select avaliable neighbors of last(local)
-    #                     selectedNeighbors = []    # type Node
-    #                     selectedNeighbors.clear()
-    #                     # print('===p2Extra neighbours === ' , last.neighbors)
-    #                     for neighbor in last.neighbors:
-    #                         if neighbor.remainingQubits >= 2 or (neighbor == dst and neighbor.remainingQubits >= 1):
-    #                             for link in neighbor.links:
-    #                                 if link.contains(last) and (not link.assigned):
-    #                                     # print('select neighbor:', neighbor.id)
-    #                                     selectedNeighbors.append(neighbor)
-    #                                     break
-
-    #                     # Choose the neighbor with smallest number of hop from it to dst
-    #                     next = self.topo.sentinel
-    #                     hopsCurMinNum = sys.maxsize
-    #                     for selectedNeighbor in selectedNeighbors:
-    #                         hopsNum = self.topo.hopsAway(selectedNeighbor, dst, 'Hop')      
-    #                         if hopsCurMinNum > hopsNum:
-    #                             hopsCurMinNum = hopsNum
-    #                             next = selectedNeighbor
-
-    #                     # If have cycle, break
-    #                     if next == self.topo.sentinel or next in p:
-    #                         break 
-    #                     p.append(next)
-    #                 # while end
-
-    #                 if p[-1] != dst:
-    #                     continue
-    #                 # print('p2Extra: ' , [n.id for n in p])
-    #                 # Caculate width for p
-    #                 width = self.topo.widthPhase2(p)
-                    
-    #                 if width == 0:
-    #                     continue
-                    
-    #                 # Assign Qubits for links in path     
-    #                 for i in range(0, len(p) - 1):
-    #                     n1 = p[i]
-    #                     n2 = p[i+1]
-    #                     for link in n1.links:
-    #                         if link.contains(n2) and (not link.assigned):
-    #                             self.totalUsedQubits += 2
-    #                             link.assignQubits()
-    #                             break 
-
-    #                 if requestInfo.state == 1:
-    #                     self.totalUsedQubits += 1
-    #                     dst.assignIntermediate()
-                    
-    #                 if tuple(p) not in requestInfo.width:
-    #                     if requestInfo.state == 2:
-    #                         requestInfo.pathseg2.append(p)
-    #                     else:
-    #                         requestInfo.pathseg1.append(p)
-    #                     requestInfo.taken= True
-    #                     requestInfo.width[tuple(p)] = 1
-    #                 else:
-    #                     requestInfo.width[tuple(p)] += 1
-
-                    
-    #                 found = True
-    #                 # print('[' , self.name, ']', ' P2Extra take')
-
-                
-    #         # for end
-    #         if not found:
-    #             break
+ 
     def resetFailedRequestFor01(self, requestInfo, usedLinks=[]):      # 第一段傳失敗
         # for link in usedLinks:
         #     link.clearPhase4Swap()
@@ -773,7 +671,6 @@ class MyAlgorithm(AlgorithmBase):
             remainTime += self.timeSlot - req[2]
         entSum = sum(self.result.entanglementPerRound)
 
-        # self.stats()
         self.topo.clearAllEntanglements()
         self.result.remainRequestPerRound.append(len(self.requestState))  
         self.result.waitingTime = (self.totalTime + remainTime) / self.totalNumOfReq + 1
@@ -791,10 +688,20 @@ class MyAlgorithm(AlgorithmBase):
         print('[MyAlgo] p5 end')
         # print('----------------------')
         self.filterReqeuest()
+        self.stats()
 
         return self.result
     def filterReqeuest(self):
-        self.requestState = {k:v for k,v in self.requestState.items() if (self.timeSlot -  k[2]) < request_timeout}
+        tmp = {}
+        for key in self.requestState:
+            req = self.requestState[key]
+            if self.timeSlot -  key[2] < request_timeout:
+                if req.state == 2:
+                    req.intermediate.clearIntermediate()
+            else:
+                tmp[key] = req
+        self.requestState = tmp
+        # self.requestState = {k:v for k,v in self.requestState.items() if (self.timeSlot -  k[2]) < request_timeout}
     
 if __name__ == '__main__':
 
--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -44,11 +44,11 @@ from agent import Agent    #for ubuntu
 from DQNAgent import DQNAgent    #for ubuntu
 # from rl.DQNAgent import Agent   #for mac
 
-ttime = 500
-step = 50
+ttime = 5000
+step = 200
 times = 6
 # numOfRequestPerRound = [1, 2, 3]
-numOfRequestPerRound = [30]
+numOfRequestPerRound = [50]
 # numOfRequestPerRound = [50 ]
 # numOfRequestPerRound = [2]
 totalRequest = [10, 20, 30, 40, 50]
@@ -103,16 +103,16 @@ def Run(numOfRequestPerRound = 20, numOfNode = 0, r = 7, q = 0.9, alpha = 0.0002
     # make copy
     algorithms = []
 
-    # algorithms.append(MyAlgorithm(copy.deepcopy(topo)))
-    # algorithms.append(SEERCACHE(copy.deepcopy(topo), param = 'ten', name='SEERCACHE'))
+    algorithms.append(MyAlgorithm(copy.deepcopy(topo)))
+    algorithms.append(SEERCACHE(copy.deepcopy(topo), param = 'ten', name='SEERCACHE'))
     # # # # # algorithms.append(SEERCACHE2(copy.deepcopy(topo), param = 'ten', name='SEER3'))
     # # # # # # algorithms.append(SEERCACHE3(copy.deepcopy(topo), param = 'ten', name='SEER4'))
 
-    # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_1hop'))
-    # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_1hop_qrl'))
+    algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_1hop'))
+    algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_1hop_qrl'))
     # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_1hop_dqrl'))
-    # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_multihop'))
-    # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_multihop_qrl'))
+    algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_multihop'))
+    algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_multihop_qrl'))
     # algorithms.append(SEERCACHE3_3(copy.deepcopy(topo), param = 'ten', name='SEER_preswap_multihop_dqrl'))
 
     #with pre entanglement
@@ -127,13 +127,13 @@ def Run(numOfRequestPerRound = 20, numOfNode = 0, r = 7, q = 0.9, alpha = 0.0002
     # #with pre entanglement
     # algorithms.append(CachedEntanglement(copy.deepcopy(topo),preEnt=True))
     
-    algorithms.append(REPS(copy.deepcopy(topo)))
-    algorithms.append(REPSCACHE(copy.deepcopy(topo),param='ten',name='REPSCACHE2'))
-    # # # algorithms.append(REPSCACHE2(copy.deepcopy(topo),param='ten',name='REPSCACHE3'))
-    # # # # # ## algorithms.append(REPSCACHE4(copy.deepcopy(topo),param='ten',name='REPSCACHE4'))
-    # # # # # # algorithms.append(REPSCACHE5(copy.deepcopy(topo),param='ten',name='REPSCACHE5'))
+    # algorithms.append(REPS(copy.deepcopy(topo)))
+    # algorithms.append(REPSCACHE(copy.deepcopy(topo),param='ten',name='REPSCACHE2'))
+    # # # # algorithms.append(REPSCACHE2(copy.deepcopy(topo),param='ten',name='REPSCACHE3'))
+    # # # # # # ## algorithms.append(REPSCACHE4(copy.deepcopy(topo),param='ten',name='REPSCACHE4'))
+    # # # # # # # algorithms.append(REPSCACHE5(copy.deepcopy(topo),param='ten',name='REPSCACHE5'))
 
-    algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPS_preswap_1hop'))
+    # algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPS_preswap_1hop'))
     # # algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPSCACHE5_preswap_multihop'))
     # algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPSCACHE5_preswap_multihop_qrl'))
     # algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPS_preswap_1hop_dqrl'))
