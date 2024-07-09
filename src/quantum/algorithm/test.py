98cab6f47cb8b579eedfb99724486ac6296530ef
Hasan Tasdiq <tasdiq088@Gmail.com>
2024-07-09 17:54:34 -0500

    40 node 40 req

--- src/quantum/algorithm/test.py
+++ src/quantum/algorithm/test.py
@@ -18,34 +18,10 @@ dp = [[0]*5]*5
 
 # print([(p , q) for (p , q) in  zip(path[0:-1] , path[1:])])
 
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-def swapPairs( head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        prev = head
-
-        current = prev.next
-        print(current.val)
-        # prevprev = None
-        # while current is not None:
-        #     prev.next = current.next
-        #     current.next = prev
-        #     prevprev = prev
-
-        #     prev = current.next
-
-head = ListNode(val= 1, next= ListNode(val= 2, next= ListNode(val= 3, next= ListNode(val= 4, next= None))))
-# current = head
-# for i in range(1 , 4):
-#      n = ListNode(i)
-#      current.next = n
-#      current = n
-
-     
-swapPairs(head)
+max_ = 0
+for i in range(10000000):
+    rand = int(random.random()*5+3) 
+#     print(rand)
+    max_ = max(max_ , rand)
+print(max_)
 
