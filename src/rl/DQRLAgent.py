df8feec6a331253820cd14a9063ec2c1e264ed63
Tasdiqul <tasdiq088@gmail.com>
2025-03-09 15:02:59 -0500

    5 15

--- src/rl/DQRLAgent.py
+++ src/rl/DQRLAgent.py
@@ -257,7 +257,7 @@ class DQRLAgent:
         # Get current states from minibatch, then query NN model for Q values
         current_states = np.array([transition[0] for transition in minibatch])
         # print(current_states)
-        current_qs_list = self.model.predict(current_states , verbose=0, batch_size=batch_size,use_multiprocessing=True)
+        current_qs_list = self.model.predict(current_states , verbose=0, batch_size=batch_size)
         print('=============current_qs_list predict ===========' , time.time() - t11)
 
         t2 = time.time()
@@ -265,7 +265,7 @@ class DQRLAgent:
         # Get future states from minibatch, then query NN model for Q values
         # When using target network, query it, otherwise main network should be queried
         new_current_states = np.array([transition[3] for transition in minibatch])
-        future_qs_list = self.target_model.predict(new_current_states , verbose=0, batch_size=batch_size,use_multiprocessing=True)
+        future_qs_list = self.target_model.predict(new_current_states , verbose=0, batch_size=batch_size)
 
         print('=============future_qs_list predict===========' , time.time() - t2)
 
@@ -325,7 +325,7 @@ class DQRLAgent:
         t4 = time.time()
         # print('=============train start===========')
         # Fit on all samples as one batch, log only on terminal state
-        hist = self.model.fit(np.array(X), np.array(y), batch_size=batch_size, verbose=0, shuffle=True, use_multiprocessing=True,)
+        hist = self.model.fit(np.array(X), np.array(y), batch_size=batch_size, verbose=0, shuffle=True,)
         print('============= total train done===========' , time.time() - t1)
         print('=============only train done===========' , time.time() - t4)
         # Update target network counter every episode
