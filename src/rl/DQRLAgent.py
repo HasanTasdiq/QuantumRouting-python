4023fb925e9594028b0eee729b587fd632979b48
Tasdiqul <tasdiq088@gmail.com>
2025-03-09 00:53:34 -0600

    10 100

--- src/rl/DQRLAgent.py
+++ src/rl/DQRLAgent.py
@@ -48,14 +48,14 @@ ENTANGLEMENT_LIFETIME = 10
 # Exploration settings
 
 EPSILON_ = 1  # not a constant, qoing to be decayed
-START_EPSILON_DECAYING = 20000
-END_EPSILON_DECAYING = 25000
+START_EPSILON_DECAYING = 7000
+END_EPSILON_DECAYING = 10000
 EPSILON_DECAY_VALUE = EPSILON_/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 
 DISCOUNT = 0.5
-REPLAY_MEMORY_SIZE = 120000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 80000  # Minimum number of steps in a memory to start training
+REPLAY_MEMORY_SIZE = 80000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 50000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 2024  # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 50  # Terminal states (end of episodes)
 FAILURE_REWARD = -2
