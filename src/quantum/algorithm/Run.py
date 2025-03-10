5f738b48e5bd083f4f4e7cc7dc17d226553b6475
Tasdiqul <tasdiq088@gmail.com>
2025-03-10 14:58:55 -0500

    10 50

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -215,7 +215,7 @@ def Run(numOfRequestPerRound = 50, numOfNode = 0, r = 7, q = 1, alpha = alpha_, 
     
 
     # algorithms.append(REPS(copy.deepcopy(topo) , name = 'REPS'))
-    # algorithms.append(REPSREP(copy.deepcopy(topo) , name = 'REPS_rep'))
+    algorithms.append(REPSREP(copy.deepcopy(topo) , name = 'REPS_rep'))
     # algorithms.append(REPSREP(copy.deepcopy(topo) , name = 'REPS_shortest'))
     # algorithms.append(REPS(copy.deepcopy(topo) , name = 'REPS', param = 'reps_ten'))
     # algorithms.append(REPSCACHE(copy.deepcopy(topo),param='ten',name='REPSCACHE2'))
@@ -232,7 +232,7 @@ def Run(numOfRequestPerRound = 50, numOfNode = 0, r = 7, q = 1, alpha = alpha_, 
     # # algorithms.append(REPSCACHE5_3(copy.deepcopy(topo),param='ten',name='REPSCACHE5_preswap_multihop_dqrl')) #working
     
     
-    algorithms.append(REPS_ENT_DQRL(copy.deepcopy(topo) , name='REPS_entdqrl'))
+    # algorithms.append(REPS_ENT_DQRL(copy.deepcopy(topo) , name='REPS_entdqrl'))
     # # algorithms.append(REPS_ENT_DQRL(copy.deepcopy(topo) , name='REPS_entdqrl_no_repeat'))
     # # algorithms.append(REPS_ENT_DQRL(copy.deepcopy(topo) , name='REPS_2entdqrl'))
     # # algorithms.append(REPS_ENT_DQRL(copy.deepcopy(topo) , name='REPS_2entdqrl_no_repeat'))
