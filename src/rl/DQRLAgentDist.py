95109d2edb607d974a91d94522a6a276af60cb7e
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-06-28 15:58:40 -0500

    run 4x4

--- src/rl/DQRLAgentDist.py
+++ src/rl/DQRLAgentDist.py
@@ -52,6 +52,16 @@ ENTANGLEMENT_LIFETIME = 10
 # Exploration settings
 
 EPSILON_ = 1  # not a constant, qoing to be decayed
+
+#run 15k
+START_EPSILON_DECAYING = 5000
+END_EPSILON_DECAYING = 12000
+REPLAY_MEMORY_SIZE = 80000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 40000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 1500  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 100  # Terminal states (end of episodes)
+
+#for 5k local
 START_EPSILON_DECAYING = 2000
 END_EPSILON_DECAYING = 4000
 REPLAY_MEMORY_SIZE = 50000  # How many last steps to keep for model training
