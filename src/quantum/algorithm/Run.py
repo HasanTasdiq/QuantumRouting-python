1ab60092f99abed974f1923538f50abc088066f0
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-07-02 14:24:46 -0500

    try 3 times

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -73,7 +73,7 @@ run = "ALPHA = " + str(ALPHA) + " BETA = " +str(BETA) + " GAMMA = "+str(GAMMA) +
 + str(SKIP_REWAD) + ' MINIBATCH_SIZE ' + str(MINIBATCH_SIZE) \
     +'REPLAY_MEMORY_SIZE' + str(REPLAY_MEMORY_SIZE)+ " reward/10 as recursive -1/e 10 -10 input without q in state+= 3 8 waxman .9q try 3"
 batchdescription = "le .0005"
-ttime = 25000
+ttime = 10000
 ttime2 = 500
 step = 500
 times = 1
