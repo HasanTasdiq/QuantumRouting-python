cd0407094cef83de56a7010592765c2085e360c1
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-07-10 18:04:15 -0500

    run diff

--- src/rl/SchedulerAgent.py
+++ src/rl/SchedulerAgent.py
@@ -34,21 +34,21 @@ EPSILON_ = 1  # not a constant, qoing to be decayed
 DISCOUNT = 0.9
 
 
-START_EPSILON_DECAYING = 2000
-END_EPSILON_DECAYING = 4000
-REPLAY_MEMORY_SIZE = 5000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 2000  # Minimum number of steps in a memory to start training
+START_EPSILON_DECAYING = 4000
+END_EPSILON_DECAYING = 8000
+REPLAY_MEMORY_SIZE = 10000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 4000  # Minimum number of steps in a memory to start training
 MINIBATCH_SIZE = 512  # How many steps (samples) to use for training
 UPDATE_TARGET_EVERY = 50  # Terminal states (end of episodes)
 
 
 # for testing
-START_EPSILON_DECAYING = 20
-END_EPSILON_DECAYING = 40
-REPLAY_MEMORY_SIZE = 500  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 200  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 10  # Terminal states (end of episodes)
+# START_EPSILON_DECAYING = 20
+# END_EPSILON_DECAYING = 40
+# REPLAY_MEMORY_SIZE = 500  # How many last steps to keep for model training
+# MIN_REPLAY_MEMORY_SIZE = 200  # Minimum number of steps in a memory to start training
+# MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+# UPDATE_TARGET_EVERY = 10  # Terminal states (end of episodes)
 
 
 
