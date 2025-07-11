68f1d7ff519a6998573f0f35944ab32c9d8484f5
Tasdiqul <tasdiq088@gmail.com>
2025-07-11 02:36:17 -0500

    more run

--- src/rl/SchedulerAgent.py
+++ src/rl/SchedulerAgent.py
@@ -34,12 +34,12 @@ EPSILON_ = 1  # not a constant, qoing to be decayed
 DISCOUNT = 0.9
 
 
-START_EPSILON_DECAYING = 4000
-END_EPSILON_DECAYING = 8000
-REPLAY_MEMORY_SIZE = 10000  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 4000  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 512  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 50  # Terminal states (end of episodes)
+START_EPSILON_DECAYING = 6000
+END_EPSILON_DECAYING = 12000
+REPLAY_MEMORY_SIZE = 50000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 20000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 1200  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 100  # Terminal states (end of episodes)
 
 
 # for testing
