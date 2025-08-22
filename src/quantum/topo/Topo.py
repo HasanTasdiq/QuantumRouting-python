42844ebe9ae9c5c25f2fe4822873e46156bcf573
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-08-21 21:39:32 -0500

    check multi thread

--- src/quantum/topo/Topo.py
+++ src/quantum/topo/Topo.py
@@ -1,3 +1,4 @@
+from multiprocessing.managers import BaseManager
 import sys
 import random
 import heapq
@@ -20,7 +21,7 @@ import traceback
 import psutil
 import os
 from concurrent.futures import ProcessPoolExecutor
-
+from multiprocessing import Manager
 
 
 
@@ -79,6 +80,7 @@ class TopoConnectionChecker:
             nextNode = link.theOtherEndOf(currentNode)
             if not self.visited[nextNode]:
                 self.DFS(nextNode)
+class MyManager(BaseManager): pass
 
 class Topo:
 
@@ -124,7 +126,7 @@ class Topo:
         self.fidelity_threshold = 0
         self.initial_fidelity = 0.9
         # self.executor = ProcessPoolExecutor(max_workers=8)
-        self.tst = list()
+        self.tst = Manager().list()  # Shared list for time slots
 
 
         # for pos in _positions:
@@ -506,6 +508,8 @@ class Topo:
         graphFileName = 'graph' + str(n) +'.pickle'
         file = 'SurfnetCore.gml'
         name = 'waxman'
+        MyManager.register("Topo", Topo)
+
         while True:
             # try:
             #     G = pickle.load(open(graphFileName, 'rb'))
