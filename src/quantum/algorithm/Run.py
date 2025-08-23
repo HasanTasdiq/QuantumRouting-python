9212f32ae3645227ec4e447007676523834b0655
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-08-22 20:13:50 -0500

    5 10

--- src/quantum/algorithm/Run.py
+++ src/quantum/algorithm/Run.py
@@ -80,11 +80,11 @@ run = "ALPHA = " + str(ALPHA) + " BETA = " +str(BETA) + " GAMMA = "+str(GAMMA) +
 + str(SKIP_REWAD) + ' MINIBATCH_SIZE ' + str(MINIBATCH_SIZE) \
     +'REPLAY_MEMORY_SIZE' + str(REPLAY_MEMORY_SIZE)+ " reward/10 as recursive -1/e 10 -10 input without q in state+= 3 8 waxman .9q try 3"
 batchdescription = "le .0005"
-ttime = 50
+ttime = 25000
 ttime2 = 500
 step = 500
 times = 1
-gridSize = 3
+gridSize = 5
 nodeNo = gridSize *gridSize
 # nodeNo = 50
 fixed = False
@@ -96,7 +96,7 @@ degree = 1
 # numOfRequestPerRound = [1, 2, 3]
 # numOfRequestPerRound = [15 , 20 , 25]
 # numOfRequestPerRound = [25,30,35]
-numOfRequestPerRound = [3]
+numOfRequestPerRound = [10]
 totalRequest = [10, 20, 30, 40, 50]
 numOfNodes = [49 , 64 , 81 , 100 ]
 # numOfNodes = [20]
--- src/rl/DQRLAgentDist.py
+++ src/rl/DQRLAgentDist.py
@@ -53,12 +53,12 @@ ENTANGLEMENT_LIFETIME = 10
 EPSILON_ = 1  # not a constant, qoing to be decayed
 
 # run 25k
-# START_EPSILON_DECAYING = 15000
-# END_EPSILON_DECAYING = 20000
-# REPLAY_MEMORY_SIZE = 100000  # How many last steps to keep for model training
-# MIN_REPLAY_MEMORY_SIZE = 50000  # Minimum number of steps in a memory to start training
-# MINIBATCH_SIZE = 1500  # How many steps (samples) to use for training
-# UPDATE_TARGET_EVERY = 100  # Terminal states (end of episodes)
+START_EPSILON_DECAYING = 15000
+END_EPSILON_DECAYING = 20000
+REPLAY_MEMORY_SIZE = 100000  # How many last steps to keep for model training
+MIN_REPLAY_MEMORY_SIZE = 50000  # Minimum number of steps in a memory to start training
+MINIBATCH_SIZE = 1500  # How many steps (samples) to use for training
+UPDATE_TARGET_EVERY = 100  # Terminal states (end of episodes)
 
 # for 5k local
 # START_EPSILON_DECAYING = 2000
@@ -78,12 +78,12 @@ EPSILON_ = 1  # not a constant, qoing to be decayed
 
 
 # for testing
-START_EPSILON_DECAYING = 10
-END_EPSILON_DECAYING = 20
-REPLAY_MEMORY_SIZE = 200  # How many last steps to keep for model training
-MIN_REPLAY_MEMORY_SIZE = 100  # Minimum number of steps in a memory to start training
-MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
-UPDATE_TARGET_EVERY = 10  # Terminal states (end of episodes)
+# START_EPSILON_DECAYING = 10
+# END_EPSILON_DECAYING = 20
+# REPLAY_MEMORY_SIZE = 200  # How many last steps to keep for model training
+# MIN_REPLAY_MEMORY_SIZE = 100  # Minimum number of steps in a memory to start training
+# MINIBATCH_SIZE = 64  # How many steps (samples) to use for training
+# UPDATE_TARGET_EVERY = 10  # Terminal states (end of episodes)
 
 EPSILON_DECAY_VALUE = EPSILON_/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
