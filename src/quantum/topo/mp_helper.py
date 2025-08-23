3ef0df2073e9fd6e0206633f65fdbe8f63e24ee2
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-08-22 20:13:50 -0500

    5 10

--- src/quantum/topo/mp_helper.py
+++ src/quantum/topo/mp_helper.py
@@ -17,8 +17,8 @@ class qManager(BaseManager): pass
 qManager.register('sNode', sNode,)
 qManager.register('sLink', sLink)
 
-executor = ProcessPoolExecutor(max_workers=8)
-executor = ThreadPoolExecutor(max_workers=8)
+# executor = ProcessPoolExecutor(max_workers=8)
+executor = ThreadPoolExecutor(max_workers=128)
 
 def route_schedule_single2(  reqState):
     print('route_schedule_single called with algo#############################################:')
