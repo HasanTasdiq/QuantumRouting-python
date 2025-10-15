ae0ee15b7314c966e8b360d63f5871aec11d288e
Hasan Tasdiq <tasdiq088@Gmail.com>
2025-10-15 17:18:43 -0500

    10 20 //2 minbacth train in processpool

--- src/quantum/algorithm/DQRL_dist1.py
+++ src/quantum/algorithm/DQRL_dist1.py
@@ -419,7 +419,7 @@ class QuRA_DQRL_DIST(AlgorithmBase):
             traceback.print_exc()
 
     async def call_update_reward(self, successful_requests: int, timeSlot: int, actions : list):
-        # print('in update_reward with ', timeSlot)
+        print('~~~~~~~~~~~in update_reward with ', timeSlot)
         t = time.time()
         url = "http://127.0.0.1:8000/update_reward"
         batch_json = []
@@ -457,7 +457,7 @@ class QuRA_DQRL_DIST(AlgorithmBase):
         except httpx.HTTPStatusError as e:
             print(f"HTTP error from update_reward API: {e.response.status_code}")
             return {"status": "error", "message": str(e)}
-        print('update_reward api call completed', time.time() - t , 'sec for timeSlot ' , timeSlot)
+        print('~~~~~~~~~~@@@@@@update_reward api call completed', time.time() - t , 'sec for $$$$$$$$$$$$ timeSlot ' , timeSlot)
     def convert_to_serializable_actions(self , batch_params):
         """Convert any NumPy arrays or NumPy scalars to Python native types."""
         serializable_batch = []
